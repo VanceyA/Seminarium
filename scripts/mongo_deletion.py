6443a4e83e7ee6e94b2c8982a11425b15c880953
VanceyA <vancevance2003@gmail.com>
2024-07-30 17:26:14 -0600

    Adding MongoDB import scripts

---
+++ scripts/mongo_deletion.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+def delete_all_documents(mongo_uri, database_name, collection_name):
+    client = MongoClient(mongo_uri)
+    db = client[database_name]
+    collection = db[collection_name]
+
+    result = collection.delete_many({})
+    print(f"Deleted {result.deleted_count} documents from {database_name}.{collection_name}")
+
+# Replace the following variables with your specific details
+mongo_uri = ""
+database_name = "seminariumDev"
+collection_name = "seminars"
+
+delete_all_documents(mongo_uri, database_name, collection_name)
---
+++ scripts/mongo_import.py
@@ -0,0 +1,25 @@
+import csv
+import json
+from pymongo import MongoClient
+
+
+def import_csv_to_mongodb(input_file, mongo_uri, database_name, collection_name):
+    client = MongoClient(mongo_uri)
+    db = client[database_name]
+    collection = db[collection_name]
+
+    with open(input_file, mode='r', encoding='utf-8-sig') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            row['fields'] = json.loads(row['fields'])
+            collection.insert_one(row)
+
+    print(f"Data imported successfully into {database_name}.{collection_name}")
+
+# Replace the following variables with your specific details
+input_csv = "Preprocessed_Seminarium_Test_Data.csv"
+mongo_uri = ""
+database_name = "seminariumDev"
+collection_name = "seminars"
+
+import_csv_to_mongodb(input_csv, mongo_uri, database_name, collection_name)
