36dc5eb9e583df4bcbcaa788a9f0ebbce150181f
VanceyA <vancevance2003@gmail.com>
2024-07-31 12:24:58 -0600

    Updating frontend to handle user state

---
+++ client/src/data/globalUserData.js
@@ -0,0 +1,5 @@
+import { reactive } from 'vue';
+
+export const globalUserData = reactive({
+    user: null
+});
--- client/src/main.js
+++ client/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import './index.css';
 import { fieldColorMap, seminarTypeColorMap } from './data/globalData';
+import { globalUserData } from './data/globalUserData';
 
 const app = createApp(App);
 
 app.provide('fieldColorMap', fieldColorMap);
 app.provide('seminarTypeColorMap', seminarTypeColorMap);
+app.provide('globalUserData', globalUserData);
 
 
 app.use(router);
