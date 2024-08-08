5b963b190dd65d648b7d4f54df05aabf40388e24
VanceyA <vancevance2003@gmail.com>
2024-08-08 11:19:27 -0600

    Adding script to list seminars

---
+++ scripts/mongo_listSeminars.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+def check_collection(mongo_uri, database_name, collection_name):
+    client = MongoClient(mongo_uri)
+    db = client[database_name]
+    collection = db[collection_name]
+    
+    documents = collection.find()
+    for document in documents:
+        print(document)
+
+mongo_uri = "mongodb://localhost:27017"
+database_name = "seminarium"
+collection_name = "seminars"
+
+check_collection(mongo_uri, database_name, collection_name)
