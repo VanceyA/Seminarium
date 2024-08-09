891e72bdb273a633ffb563b86bb1d758345bced5
VanceyA <vancevance2003@gmail.com>
2024-08-09 11:16:06 -0600

    Taking down test endpoints

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
@@ -18,45 +18,45 @@ mongoose.connect(process.env.MONGO_URI);
 
 const port = process.env.PORT || 8080;
 
-function verifyToken(req, res, next) {
-    const token = req.headers['authorization'].split(' ')[1];
+// function verifyToken(req, res, next) {
+//     const token = req.headers['authorization'].split(' ')[1];
     
-    if (!token) {
-        return res.status(403).send('A token is required for authentication');
-    }
+//     if (!token) {
+//         return res.status(403).send('A token is required for authentication');
+//     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        req.user = decoded;
-    } catch (err) {
-        return res.status(401).send('Invalid Token');
-    }
-    next();
-}
+//     try {
+//         const decoded = jwt.verify(token, process.env.SECRET_KEY);
+//         req.user = decoded;
+//     } catch (err) {
+//         return res.status(401).send('Invalid Token');
+//     }
+//     next();
+// }
 
 // Routes
 app.use('/api/seminars', require('./routes/seminarRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/schedule', require('./routes/scheduleRoutes'));
 
-app.get('/api/get-jwt', (req, res) => {
-    const secret_key = process.env.SECRET_KEY;
-    const payload = {
-        studentID: "00111111",
-        name: "John Smith"
-    };
-    const options = {
-        expiresIn: '1h'
-    };
+// app.get('/api/get-jwt', (req, res) => {
+//     const secret_key = process.env.SECRET_KEY;
+//     const payload = {
+//         studentID: "00111111",
+//         name: "John Smith"
+//     };
+//     const options = {
+//         expiresIn: '1h'
+//     };
 
-    const token = jwt.sign(payload, secret_key, options);
-    res.json({ token });
-});
+//     const token = jwt.sign(payload, secret_key, options);
+//     res.json({ token });
+// });
 
 
-app.get('/api/login', verifyToken, (req, res) => {
-    res.json(req.user);
-});
+// app.get('/api/login', verifyToken, (req, res) => {
+//     res.json(req.user);
+// });
 
 // For static files, use index.html (ran into issues with refreshing pages without this) (vue-router compatability)
 app.get('*', (req, res) => {
